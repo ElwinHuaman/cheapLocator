b3e9d2aee56b857c4cf3bfd57228a22a8933cdac
Elwin <elwinlhq@hotmail.com>
2015-09-21 11:16:24 +0200

    Key Google maps
    Probando la URL y el Key

--- js/motor.js
+++ js/motor.js
@@ -6,5 +6,33 @@ function initialize(){
 		zoom: 15,
 		mapTypeId: google.maps.MapTypeId.ROADMAP
 	};
+
 	map = new google.maps.Map(document.getElementById("mainMap"),opciones);
+
+	var infoWindow = new google.maps.InfoWindow({map: map});
+
+	//Geolocalizacion con HTML5
+	if (navigator.geolocation) {
+		navigator.geolocation.getCurrentPosition(function(position) {
+		var pos = {
+				lat: position.coords.latitude,
+				lng: position.coords.longitude
+			};
+			infoWindow.setPosition(pos);
+			infoWindow.setContent('Tu ubicacion.');
+			map.setCenter(pos);
+		}, function(){
+			handleLocationError(true, infoWindow, map.getCenter());
+		});
+	} else {
+		//Tu navegador no soporta geolocalizacion
+		handleLocationError(false, infoWindow, map.getCenter());
+	}
+}
+
+function handleLocationError(browserHasGeolocation, infoWindow, pos){
+	infoWindow.setPosition(pos);
+	infoWindow.setContent(browserHasGeolocation ?
+		'Error: El servicio de geolocalizacion ha fallado.' :
+		'Error: Tu Navegador no soporta geolocalizacion');
 }
